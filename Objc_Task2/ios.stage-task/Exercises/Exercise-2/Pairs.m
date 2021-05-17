ce82598ed9d96b3ecb8102d14861c16b685a81e0
Kobzarev Roman <krv2306@gmail.com>
2021-05-17 23:52:00 +0300

    fix: change 2 task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,11 +3,13 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    NSInteger countPairs = 0;
     
-    for (NSInteger i = 0; i < array.count; i++) {
-        for (NSInteger j = 0; j < array.count; j++) {
-            if (array[i].integerValue + number.integerValue == array[j].integerValue){
+    NSInteger countPairs = 0;
+    NSInteger lenghtArr = array.count;
+    
+    for (int i = 0; i < lenghtArr; i++) {
+        for (int j = i+1; j < lenghtArr; j++) {
+            if (abs([array[i] intValue] + [number intValue]) == [array[j] intValue]){
                 countPairs += 1;
             }
         }
